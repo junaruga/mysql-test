186688eb2a79edd6340de785ab1f89ae2261ddb4
Jun Aruga <jaruga@redhat.com>
2021-04-11 20:58:41 +0200

    Comment out some commands in ci/setup.sh to avoid stucking.

--- ci/setup.sh
+++ ci/setup.sh
@@ -5,11 +5,11 @@ set -eux
 DB="${DB-}"
 
 sudo service mysql stop
-sudo apt-get purge -qq '^mysql*' '^libmysql*'
-sudo rm -fr /etc/mysql
-sudo rm -fr /var/lib/mysql
+# sudo apt-get purge -qq '^mysql*' '^libmysql*'
+# sudo rm -fr /etc/mysql
+# sudo rm -fr /var/lib/mysql
 
 sudo apt-get update
 sudo apt-get install -qq "${DB}"
 
-sudo service mariadb status
+# sudo service mariadb status
