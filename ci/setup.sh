be5e6f690238a8334c284e27e5c3267e96392231
Jun Aruga <jaruga@redhat.com>
2021-04-11 20:45:54 +0200

    Fix issues.

--- ci/setup.sh
+++ ci/setup.sh
@@ -4,7 +4,12 @@ set -eux
 
 DB="${DB-}"
 
+sudo service mysql stop
+sudo apt-get purge -qq '^mysql*' '^libmysql*'
+sudo rm -fr /etc/mysql
+sudo rm -fr /var/lib/mysql
+
 sudo apt-get update
 sudo apt-get install -qq "${DB}"
 
-systemctl status mariadb
+sudo service mariadb status
